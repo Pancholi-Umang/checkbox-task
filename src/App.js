98a20cd0ea70a2f3a639161daf789d4664a5f16b
unknown <umangpancholi555@gmail.com>
2023-06-16 12:10:28 +0530

    make successfull task

--- src/App.js
+++ src/App.js
@@ -1,104 +1,34 @@
-// import { useEffect, useState } from "react";
-// import "./App.css";
+// import React, { useState } from "react";
 
-// function App() {
-//   const arr = [
-//     [1990, 1991, 1992, 1993, 1994],
-//     [2000, 2001, 2002, 2003, 2004],
-//     [2010, 2011, 2012],
-//     [3001, 3002, 3003, 3004]
-//   ]
-
-//   const [boxIsCheck, setBoxIsCheck] = useState([]);
-
-//   const onHandleChange = (index, i) => {
-//     var updatedList = [...boxIsCheck];
-//     var check = boxIsCheck?.includes(arr[index][i])
-
-//     if (check) {
-//       updatedList.splice(boxIsCheck.indexOf(arr[index][i]), 1);
-//     } else {
-//       updatedList = [...boxIsCheck, arr[index][i]]
-//     }
-//     setBoxIsCheck(updatedList)
-//   }
-
-//   const [save, setSave] = useState([])
-//   useEffect(() => {
-//     const trueIndices = [];
-//     for (let i = 0; i < arr.length; i++) {
-//       let data = arr[i].every(value => {
-//         return boxIsCheck.includes(value);
-//       })
-//       if (data) {
-//         trueIndices.push(i)
-//       }
-//     }
-//     setSave(trueIndices)
-//   }, [boxIsCheck])
-
-//   console.log(save)
-
-//   // const onCheck = (index) => {
-//   //   const allChecked = arr[index].every((values) => values.checked);
-//   //   const updatedArr = arr.map((item, i) => {
-//   //     if (i === index) {
-//   //       return item.map((values) => ({ ...values, checked: !allChecked }));
-//   //     }
-//   //     return item;
-//   //   });
-//   //   setBoxIsCheck(updatedArr);
-//   // };
-
-//   return (
-//     <>
-
-//       {arr.map((item, index) => (
-//         <div key={index}>
-//           <input value={item} type="checkbox"/>
-//           {
-//             item?.map((values, i) => (
-//               <span key={i}>
-//                 <input value={values} type="checkbox" onChange={() => onHandleChange(index, i)} />{values}
-//               </span>
-//             ))
-//           }
-//         </div>
-//       ))}
-
-//     </>
-//   );
-// }
-
-// export default App;
-
-// import React, { useState } from 'react';
-
-// const ParentChildCheckbox = () => {
+// const App = () => {
 //   const [checkboxState, setCheckboxState] = useState([
 //     [false, false, false, false, false],
 //     [false, false, false, false, false],
 //     [false, false, false],
-//     [false, false, false, false]
+//     [false, false, false, false],
 //   ]);
 
 //   const arr = [
-//         [1990, 1991, 1992, 1993, 1994],
-//         [2000, 2001, 2002, 2003, 2004],
-//         [2010, 2011, 2012],
-//         [3001, 3002, 3003, 3004]
-//       ]
+//     [1990, 1991, 1992, 1993, 1994],
+//     [2000, 2001, 2002, 2003, 2004],
+//     [2010, 2011, 2012],
+//     [3001, 3002, 3003, 3004],
+//   ];
 
-//   const handleParentCheckboxChange = (rowIndex,rowind) => {
-//     console.log(rowind)
+//   const ParentCheckBox = (rowIndex, rowind) => {
 //     const updatedState = [...checkboxState];
+//     const updateList = [];
 //     const rowChecked = updatedState[rowIndex].every((checked) => checked);
-//     updatedState[rowIndex] = rowChecked ? updatedState[rowIndex].map(() => false) : updatedState[rowIndex].map(() => true);
+//     updatedState[rowIndex] = rowChecked
+//       ? updatedState[rowIndex].map(() => false)
+//       : updatedState[rowIndex].map(() => true);
 //     setCheckboxState(updatedState);
+//     console.log(updateList)
 //   };
 
-//   const handleChildCheckboxChange = (rowIndex, childIndex) => {
+//   const childcheckBox = (rowIndex, childIndex, colIndex) => {
 //     const updatedState = [...checkboxState];
+
 //     updatedState[rowIndex][childIndex] = !updatedState[rowIndex][childIndex];
 //     setCheckboxState(updatedState);
 //   };
@@ -110,15 +40,17 @@
 //           <input
 //             type="checkbox"
 //             checked={row.every((checked) => checked)}
-//             onChange={() => handleParentCheckboxChange(rowIndex,arr[rowIndex])}
-//           />
+//             onChange={() => ParentCheckBox(rowIndex, arr[rowIndex])}
+//           />  
 
 //           {row.map((checked, childIndex) => (
 //             <span key={childIndex}>
 //               <input
 //                 type="checkbox"
 //                 checked={checked}
-//                 onChange={() => handleChildCheckboxChange(rowIndex, childIndex)}
+//                 onChange={() =>
+//                   childcheckBox(rowIndex, childIndex, arr[rowIndex][childIndex])
+//                 }
 //               />
 //               {arr[rowIndex][childIndex]}
 //             </span>
@@ -129,89 +61,11 @@
 //   );
 // };
 
-// export default ParentChildCheckbox;
-
-// import { useEffect, useState } from 'react';
-// import './App.css';
-
-// function App() {
-//   const arr = [
-//     [1990, 1991, 1992, 1993, 1994],
-//     [2000, 2001, 2002, 2003, 2004],
-//     [2010, 2011, 2012],
-//     [3001, 3002, 3003, 3004]
-//   ];
-
-//   const [boxIsCheck, setBoxIsCheck] = useState([]);
-
-//   const onHandleChange = (index, i) => {
-//     const updatedList = [...boxIsCheck];
-//     const check = updatedList.includes(arr[index][i]);
-
-//     if (check) {
-//       updatedList.splice(updatedList.indexOf(arr[index][i]), 1);
-//     } else {
-//       updatedList.push(arr[index][i]);
-//     }
-//     setBoxIsCheck(updatedList);
-//   };
-
-//   const [save, setSave] = useState([]);
-//   useEffect(() => {
-//     const trueIndices = [];
-//     for (let i = 0; i < arr.length; i++) {
-//       const data = arr[i].every(value => boxIsCheck.includes(value));
-//       if (data) {
-//         trueIndices.push(i);
-//       }
-//     }
-//     setSave(trueIndices);
-//   }, [boxIsCheck]);
-
-//   const handleParentCheckboxChange = (index) => {
-//     const selectedValues = arr[index];
-//     const updatedList = [...boxIsCheck];
-//     const allChecked = selectedValues.every(value => updatedList.includes(value));
-
-//     if (allChecked) {
-//       updatedList.splice(updatedList.findIndex(value => selectedValues.includes(value)),selectedValues.length);
-//     } else {
-//       updatedList.push(...selectedValues);
-//     }
-
-//     setBoxIsCheck(updatedList);
-//   };
-
-//   return (
-//     <>
-//       {arr.map((item, index) => (
-//         <div key={index}>
-//           <input
-//             type="checkbox"
-//             checked={item.every(value => boxIsCheck.includes(value))}
-//             onChange={() => handleParentCheckboxChange(index)}
-//           />
-//           {item.map((value, i) => (
-//             <span key={i}>
-//               <input
-//                 type="checkbox"
-//                 checked={boxIsCheck.includes(value)}
-//                 onChange={() => onHandleChange(index, i)}
-//               />
-//               {value}
-//             </span>
-//           ))}
-//         </div>
-//       ))}
-//     </>
-//   );
-// }
-
 // export default App;
 
 import React, { useState } from "react";
 
-const ParentChildCheckbox = () => {
+const App = () => {
   const [checkboxState, setCheckboxState] = useState([
     [false, false, false, false, false],
     [false, false, false, false, false],
@@ -226,9 +80,7 @@ const ParentChildCheckbox = () => {
     [3001, 3002, 3003, 3004],
   ];
 
-  const [showValue, setShowValue] = useState([]);
-
-  const handleParentCheckboxChange = (rowIndex, rowind) => {
+  const ParentCheckBox = (rowIndex) => {
     const updatedState = [...checkboxState];
     const rowChecked = updatedState[rowIndex].every((checked) => checked);
     updatedState[rowIndex] = rowChecked
@@ -237,29 +89,36 @@ const ParentChildCheckbox = () => {
     setCheckboxState(updatedState);
   };
 
-  const handleChildCheckboxChange = (rowIndex, childIndex, colIndex) => {
+  const childcheckBox = (rowIndex, childIndex) => {
     const updatedState = [...checkboxState];
     updatedState[rowIndex][childIndex] = !updatedState[rowIndex][childIndex];
     setCheckboxState(updatedState);
   };
 
+  const [RetriveValuesOfItems, setRetriveValuesOfItems] = useState([])
+
+  const getCheckedValues = () => {
+    const checkedValues = [];
+    checkboxState.forEach((row, rowIndex) => {
+      row.forEach((checked, childIndex) => {
+        if (checked) {
+          checkedValues.push(arr[rowIndex][childIndex]);
+        }
+      });
+    });
+    setRetriveValuesOfItems(checkedValues)
+  };
+
+  
+
   return (
     <div>
-      {/* {
-        showValue?.map((val)=>{
-          return(
-            <>
-              <ul>{val+""}</ul> <br />
-            </>
-          )
-        })
-      } */}
       {checkboxState.map((row, rowIndex) => (
         <div key={rowIndex}>
           <input
             type="checkbox"
             checked={row.every((checked) => checked)}
-            onChange={() => handleParentCheckboxChange(rowIndex, arr[rowIndex])}
+            onChange={() => ParentCheckBox(rowIndex)}
           />
 
           {row.map((checked, childIndex) => (
@@ -267,15 +126,25 @@ const ParentChildCheckbox = () => {
               <input
                 type="checkbox"
                 checked={checked}
-                onChange={() => handleChildCheckboxChange(rowIndex, childIndex,arr[rowIndex][childIndex])}
+                onChange={() => childcheckBox(rowIndex, childIndex)}
               />
               {arr[rowIndex][childIndex]}
             </span>
           ))}
         </div>
       ))}
+
+      <button onClick={getCheckedValues}>submit</button>
+       
+      <h3>New Values:</h3>
+      {
+        RetriveValuesOfItems?.map((val,index)=>(
+          <span key={index}><span key={val}>{val}</span>&nbsp;</span>
+        ))
+      }
+
     </div>
   );
 };
 
-export default ParentChildCheckbox;
+export default App;
